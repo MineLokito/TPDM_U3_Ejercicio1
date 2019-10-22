f41c9825acb78b744573155ad45ce1febe0085a0
MineLokito <kealvillegases@ittepic.edu.mx>
2019-10-21 19:57:46 -0600

    Le agrege el Actualizar

--- app/src/main/java/mx/edu/ittepic/tpdm_u3_ejercicio1/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/tpdm_u3_ejercicio1/MainActivity.kt
@@ -1,12 +1,12 @@
 package mx.edu.ittepic.tpdm_u3_ejercicio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var descripcion: EditText?=null
@@ -50,6 +50,47 @@ class MainActivity : AppCompatActivity() {
                 }
             limpiarCampos()
 
+        }
+        baseRemota.collection("eventos")
+            .addSnapshotListener { querySnapshot, e ->
+                if (e!=null){
+                    Toast.makeText(this,"No hay acceso a los datos",Toast.LENGTH_SHORT).show()
+                    return@addSnapshotListener
+                }
+                registros.clear()
+                keys.clear()
+                for (documents in querySnapshot!!){
+                    var cadena= "${documents.getString("descripcion")}\n${documents.getString("fecha")}--${documents.getString("lugar")}"
+                    registros.add(cadena)
+                    keys.add(documents.id)
+                }
+                if(registros.size==0){
+                    registros.add("no hay datos Capturados")
+                }
+                var adapter=ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,registros)
+                lista?.adapter=adapter
+            }
+
+        lista.setOnItemClickListener { parent, view, position, id ->
+            AlertDialog.Builder(this).setTitle("Atencion").setMessage("Que desea hacer con \n ${registros.get(position)}?")
+                .setPositiveButton("Eliminar"){dialogInterface, wich ->
+                    baseRemota.collection("eventos").document(keys.get(position)).delete()
+                        .addOnSuccessListener {
+                            Toast.makeText(this,"se pudo eliminar",Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener{
+                            Toast.makeText(this,"No se pudo Eliminar",Toast.LENGTH_SHORT).show()
+                        }
+                }
+                .setNegativeButton("Actualizar"){dialogInterface, wich ->
+
+                    var nuevaVentana =
+                        Intent(this, Main2Activity::class.java)
+                    nuevaVentana.putExtra("id",keys.get(position) )
+                    startActivity(nuevaVentana)
+                }
+                .setNeutralButton("Cancelar"){dialogInterface, wich ->  }
+                .show()
         }
 
         }
